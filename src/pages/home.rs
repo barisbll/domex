d9988fa13eb9938b2c0dd04215ce8f3975a694eb
Baris Balli <barisbll.dev@gmail.com>
2024-09-09 20:41:48 +0200

    initial home screen added

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -8,7 +8,7 @@ pub fn home() -> Html {
             <div class="absolute top-0 left-0 w-full h-full z-[0]">
                 <video autoplay={true} loop={true} muted={true} class="w-full h-full object-cover">
                     <source src="/static/video/header_construction_door.mp4" type="video/mp4" />
-                    { "Your browser does not support the video tag." }
+                    { "Twoja przeglądarka nie obsługuje tagu wideo." }
                 </video>
             </div>
 
@@ -21,21 +21,21 @@ pub fn home() -> Html {
                 <nav class="navbar bg-base-100 w-full py-4 bg-transparent">
                     <div class="navbar-start pl-8">
                         <div class="flex-1">
-                            <a class="btn btn-ghost text-xl">{"DOMEX"}</a>
+                            <a href="/" class="btn btn-ghost text-2xl">{"DOMEX"}</a>
                         </div>
                     </div>
-                    <ul class="navbar-end gap-3 pr-8">
-                        <li><a href="/" class="text-white text-xl font-bold">{"Home"}</a></li>
-                        <li><a href="/products" class="text-white text-xl font-bold">{"Products"}</a></li>
-                        <li><a href="/about-us" class="text-white text-xl font-bold">{"About Us"}</a></li>
-                        <li><a href="/contact" class="text-white text-xl font-bold">{"Contact"}</a></li>
+                    <ul class="navbar-end gap-5 pr-8">
+                        <li><a href="/" class="text-white text-xl font-bold">{"Strona Główna"}</a></li>
+                        <li><a href="/products" class="text-white text-xl font-bold">{"Produkty"}</a></li>
+                        <li><a href="/about-us" class="text-white text-xl font-bold">{"O Nas"}</a></li>
+                        <li><a href="/contact" class="text-white text-xl font-bold">{"Kontakt"}</a></li>
                     </ul>
                 </nav>
 
                 // Text in the middle of the video
                 <div class="flex flex-col justify-center items-center text-center flex-grow">
-                    <h1 class="text-5xl text-white font-extrabold">{"Welcome to Our Website"}</h1>
-                    <p class="text-2xl text-gray-200 mt-4">{"Innovative Solutions for Your Business"}</p>
+                <h1 class="text-5xl text-white font-extrabold">{"Drzwi, które Cię wyróżnią"}</h1>
+                <p class="text-2xl text-gray-200 mt-4">{"Zainwestuj w styl i bezpieczeństwo z naszymi nowoczesnymi rozwiązaniami dla Twojego domu"}</p>
                 </div>
             </div>
         </div>
