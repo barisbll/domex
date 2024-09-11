242adaf9c87df05def2b981ad6458f9f87742d76
Baris Balli <barisbll.dev@gmail.com>
2024-09-11 19:24:18 +0200

    working navbar

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,41 +1,8 @@
-use wasm_bindgen::JsCast;
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[function_component(NavBar)]
 pub fn navbar() -> Html {
     let menu_visible = use_state(|| false);
-
-    // Use effect to toggle the menu based on the state of the checkbox
-    use_effect_with(menu_visible.clone(), move |menu_visible| {
-        // Get the document
-        let document = gloo::utils::document();
-
-        // Get the checkbox element by its id and cast to HtmlInputElement
-        let menu_toggle = document
-            .get_element_by_id("menu-toggle")
-            .unwrap()
-            .unchecked_into::<HtmlInputElement>();
-
-        // Get the mobile menu element by its id
-        let mobile_menu = document.get_element_by_id("mobile-menu").unwrap();
-
-        if **menu_visible {
-            // If the menu should be visible, check the checkbox and display the mobile menu
-            menu_toggle.set_checked(true);
-            mobile_menu.set_class_name("lg:hidden absolute top-16 left-0 w-full bg-base-100 p-4");
-        // Remove the 'hidden' class
-        } else {
-            // If the menu should be hidden, uncheck the checkbox and hide the mobile menu
-            menu_toggle.set_checked(false);
-            mobile_menu
-                .set_class_name("lg:hidden absolute top-16 left-0 w-full bg-base-100 p-4 hidden");
-            // Add the 'hidden' class
-        }
-
-        // No cleanup needed, return ()
-        ()
-    });
 
     let toggle_menu = {
         let menu_visible = menu_visible.clone();
@@ -47,35 +14,31 @@ pub fn navbar() -> Html {
             <div class="navbar-start pl-8">
                 <a href="/" class="btn btn-ghost text-2xl">{"DOMEX"}</a>
             </div>
+
             <div class="navbar-end pr-8">
+                // Mobile hamburger menu button using DaisyUI dropdown
+                <div class="dropdown dropdown-end lg:hidden">
+                    <label tabindex="0" class="btn btn-square btn-ghost text-white" onclick={toggle_menu.clone()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </label>
 
-            // Mobile hamburger menu button
-            <div class="lg:hidden">
-                <label for="menu-toggle" class="btn btn-square btn-ghost text-white" onclick={toggle_menu.clone()}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                </label>
-            </div>
+                    // Dropdown menu for mobile
+                    <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="/" class="text-xl font-bold">{"Strona Główna"}</a></li>
+                        <li><a href="/products" class="text-xl font-bold">{"Produkty"}</a></li>
+                        <li><a href="/about-us" class="text-xl font-bold">{"O Nas"}</a></li>
+                        <li><a href="/contact" class="text-xl font-bold">{"Kontakt"}</a></li>
+                    </ul>
+                </div>
 
-            // Menu items
-            <ul id="menu-items" class="hidden lg:flex lg:gap-5">
-            <li><a href="/" class="text-white text-xl font-bold">{"Strona Główna"}</a></li>
-            <li><a href="/products" class="text-white text-xl font-bold">{"Produkty"}</a></li>
-            <li><a href="/about-us" class="text-white text-xl font-bold">{"O Nas"}</a></li>
-            <li><a href="/contact" class="text-white text-xl font-bold">{"Kontakt"}</a></li>
-            </ul>
-            </div>
-
-
-            // Mobile menu for small screens
-            <input type="checkbox" id="menu-toggle" class="hidden" />
-            <div class="lg:hidden absolute top-16 left-0 w-full bg-base-100 p-4 hidden" id="mobile-menu">
-                <ul class="flex flex-col gap-4">
-                    <li><a href="/" class="text-xl font-bold">{"Strona Główna"}</a></li>
-                    <li><a href="/products" class="text-xl font-bold">{"Produkty"}</a></li>
-                    <li><a href="/about-us" class="text-xl font-bold">{"O Nas"}</a></li>
-                    <li><a href="/contact" class="text-xl font-bold">{"Kontakt"}</a></li>
+                // Desktop menu items
+                <ul id="menu-items" class="hidden lg:flex lg:gap-5">
+                    <li><a href="/" class="text-white text-xl font-bold">{"Strona Główna"}</a></li>
+                    <li><a href="/products" class="text-white text-xl font-bold">{"Produkty"}</a></li>
+                    <li><a href="/about-us" class="text-white text-xl font-bold">{"O Nas"}</a></li>
+                    <li><a href="/contact" class="text-white text-xl font-bold">{"Kontakt"}</a></li>
                 </ul>
             </div>
         </nav>
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -19,8 +19,8 @@ pub fn home() -> Html {
                 <NavBar />
 
                 <div class="flex flex-col justify-center items-center text-center flex-grow">
-                    <h1 class="text-5xl text-white font-extrabold">{"Drzwi, które Cię wyróżnią"}</h1>
-                    <p class="text-2xl text-gray-200 mt-4">{"Zainwestuj w styl i bezpieczeństwo z naszymi nowoczesnymi rozwiązaniami dla Twojego domu"}</p>
+                    <h1 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white font-extrabold">{"Drzwi, które Cię wyróżnią"}</h1>
+                    <p class="text-md md:text-lg lg:text-xl xl:text-2xl text-gray-200 mt-4">{"Zainwestuj w styl i bezpieczeństwo z naszymi nowoczesnymi rozwiązaniami dla Twojego domu"}</p>
                 </div>
             </div>
         </div>
