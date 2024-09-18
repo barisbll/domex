bb5546da39859f73af4188808e5c6fd2c6975a4f
Baris Balli <barisbll.dev@gmail.com>
2024-09-18 20:15:53 +0200

    Finished initial version

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -2,19 +2,19 @@ use yew::prelude::*;
 
 use crate::{
     components::{footer::Footer, navbar::NavBar},
-    views::home::write_us::WriteUs,
+    views::home::{call_us::CallUs, write_us::WriteUs},
 };
 
 #[function_component(Contact)]
 pub fn contact() -> Html {
     html! {
         <div>
-        <NavBar />
-        <div>
-            <h1></h1>
+            <NavBar />
+            <CallUs />
+            <div class="my-16"></div>
+            <WriteUs />
+            <div class="my-16"></div>
+            <Footer />
         </div>
-        <WriteUs />
-        <Footer />
-    </div>
     }
 }
